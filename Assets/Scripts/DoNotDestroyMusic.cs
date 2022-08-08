4e3173320f69a09fbaa292b937dbc4f54193286b
Zainian-Vermeulen <60093849+Zainian-Vermeulen@users.noreply.github.com>
2022-08-08 17:22:12 +0200

    Working on Cleanup
    Added comments to donotdestroymusic
    Deleted old script
    Started enemy

--- Assets/Scripts/DoNotDestroyMusic.cs
+++ Assets/Scripts/DoNotDestroyMusic.cs
@@ -2,23 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This script is used to not destroy the background music when switching scenes
+/// </summary>
+
 public class DoNotDestroyMusic : MonoBehaviour
 {
-    //private void Awake()
-    //{
-    //    GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
-
-    //    if (musicObj.Length > 1)
-    //    {
-    //        Destroy(this.gameObject);
-    //    }
-    //    DontDestroyOnLoad(this.gameObject);
-
-    //}
-
     private static DoNotDestroyMusic instance = null;
-    public static DoNotDestroyMusic Instance
-    {
+    
+    public static DoNotDestroyMusic Instance {
         get { return instance; }
     }
 
@@ -27,31 +19,12 @@ public class DoNotDestroyMusic : MonoBehaviour
         if (instance != null && instance != this) {
             Destroy(this.gameObject);
             return;
-        } else
-        {
+        } 
+        else {
             instance = this;
         }
+
         DontDestroyOnLoad(this.gameObject);
     }
 
-
-    /*
-     * music:
-     * https://youtu.be/RLXr_miMxHU
-     * 
-     * game win:
-     * https://freesound.org/people/jobro/sounds/60444/
-     * 
-     * game loose:
-     * https://freesound.org/people/Jofae/sounds/364929/
-     * 
-     * monster die:
-     * https://freesound.org/people/vox_artist/sounds/513621/
-     * 
-     * shoot:
-     * https://freesound.org/people/igroglaz/sounds/593857/
-     * 
-     * click button:
-     * https://freesound.org/people/Snapper4298/sounds/183473/
-     */
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,31 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
+/// <summary>
+/// This script is on the enemy prefab and it handles the movement and collision of the enemy.
+/// </summary>
 public class Enemy : MonoBehaviour
 {
-    public GameObject _bulletPrefab;
-    public event System.Action targetHit;
-
-    private Rigidbody2D rb;
-
     [SerializeField]
     private PrefabHandler prefabHandler;
-
-    public GameObject _player;
-
-    [SerializeField]
-    private GameObject gameOver;
-
-    public event System.Action GameOver;
-
-    private SceneManager sceneManager;
-    private int currentScene;
 
     [SerializeField]
     private Animator _enemyAnimator;
 
-    
+    [SerializeField]
+    private GameObject gameOver;
+
+    public GameObject _bulletPrefab;
+    public GameObject _player;
+
+    public event System.Action targetHit;
+    public event System.Action GameOver;
+
+    private Rigidbody2D rb;
+    private int currentScene;
 
     private void Start()
     {
@@ -36,52 +33,39 @@ public class Enemy : MonoBehaviour
         _enemyAnimator = GetComponent<Animator>();
 
 
-        if (currentScene == 2)
-        {
+        if (currentScene == 2) {
             prefabHandler = FindObjectOfType<PrefabHandler>();
         }
-
-       
-
+     
         gameOver = GameObject.FindGameObjectWithTag("GameOver");
        
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = transform.up * /*Time.deltaTime **/ 15f;
-
+        rb.velocity = transform.up * 15f;
     }
 
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         _bulletPrefab = GameObject.FindGameObjectWithTag("Bullet");
-        
-        
 
         gameOver = GameObject.FindGameObjectWithTag("GameOver");
 
-        if (gameOver.CompareTag(other.tag))
-        {        
+        if (gameOver.CompareTag(other.tag)){        
             GameOver?.Invoke();            
         }
 
-        if (_bulletPrefab != null)
-        {
-            if (_bulletPrefab.CompareTag(other.tag))
-            {
-                //_enemyAnimator.SetTrigger("Die");
-
+        if (_bulletPrefab != null){
+            if (_bulletPrefab.CompareTag(other.tag)){
                 targetHit?.Invoke();
             }            
         }
+        else
            return;      
     }
 
     private void Update()
     {
-
-        ApplyMovement();
-
-        
+        ApplyMovement();     
     }
 
     private void ApplyMovement()
@@ -91,10 +75,10 @@ public class Enemy : MonoBehaviour
 
         if (currentScene == 2)
         {
-            rb.velocity = -transform.right * /*Time.deltaTime **/ 0.5f;
+            rb.velocity = -transform.right * 0.5f;
         }
         else  
-        rb.velocity = -transform.right * /*Time.deltaTime **/ 2f;
+        rb.velocity = -transform.right * 2f;
     }
 
 }
